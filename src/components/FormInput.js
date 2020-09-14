a33db2b700e1293e7a92e3539d110cbd18da6cf3
doxuanhoa <57820538+doxuanhoa@users.noreply.github.com>
2020-09-14 09:47:04 +0700

    Update 14/09
    New Update

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Form as Final_Form, Field } from "react-final-form";
+import { Form as Final_Form } from "react-final-form";
 import Styles from "./Styles";
 import "rsuite/dist/styles/rsuite-default.css";
-import { Input, ControlLabel, Button, Form, InputGroup, Icon } from "rsuite";
+import { ControlLabel, Button, Form } from "rsuite";
+import InputElement from "./InputElement";
 
 export default class FormInput extends Component {
   constructor(props) {
@@ -15,22 +16,6 @@ export default class FormInput extends Component {
       onSubmitData(obj);
       setTimeout(form.restart, 0);
     };
-    const required = (value) => (value ? undefined : "(*)");
-    const mustBeNumber = (value) => (isNaN(value) ? "(*) Number" : undefined);
-    const minValue = (min) => (value) =>
-      isNaN(value) || value >= min
-        ? undefined
-        : `Should be greater than ${min}`;
-    const composeValidators = (...validators) => (value) =>
-      validators.reduce(
-        (error, validator) => error || validator(value),
-        undefined
-      );
-    const styles = {
-      textbox: {
-        width: "200",
-      },
-    };
 
     return (
       <Styles>
@@ -40,96 +25,19 @@ export default class FormInput extends Component {
             initialValues={this.props.initialValues}
             render={({ handleSubmit, form, submitting, pristine }) => (
               <Form onSubmit={handleSubmit}>
-                <ControlLabel>Product Management</ControlLabel>
-                <Field name="id" validate={required}>
-                  {({ input, meta }) => (
-                    <div>
-                      <ControlLabel>ID</ControlLabel>
-                      <InputGroup style={styles.textbox}>
-                        <InputGroup.Addon>
-                          <Icon icon="id-info" />
-                        </InputGroup.Addon>
-                        <Input
-                          {...input}
-                          type="text"
-                          disabled={disableInputID}
-                          placeholder="ID"
-                        />
-                      </InputGroup>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                <Field name="name" validate={required}>
-                  {({ input, meta }) => (
-                    <div>
-                      <ControlLabel>Name</ControlLabel>
-                      <InputGroup style={styles}>
-                        <InputGroup.Addon>
-                          <Icon icon="slack" />
-                        </InputGroup.Addon>
-                        <Input {...input} type="text" placeholder="Name" />
-                      </InputGroup>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                <Field name="manufacturer" validate={required}>
-                  {({ input, meta }) => (
-                    <div>
-                      <ControlLabel>Manufacturer</ControlLabel>
-                      <InputGroup style={styles}>
-                        <InputGroup.Addon>
-                          <Icon icon="home" />
-                        </InputGroup.Addon>
-                        <Input
-                          {...input}
-                          type="text"
-                          placeholder="Manufacture"
-                        />
-                      </InputGroup>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                <Field
-                  name="price"
-                  validate={composeValidators(
-                    required,
-                    mustBeNumber,
-                    minValue(0)
-                  )}
-                >
-                  {({ input, meta }) => (
-                    <div>
-                      <ControlLabel>Price</ControlLabel>
-                      <InputGroup style={styles}>
-                        <InputGroup.Addon>
-                          <Icon icon="usd" />
-                        </InputGroup.Addon>
-                        <Input {...input} type="text" placeholder="Price" />
-                      </InputGroup>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-                <Field name="country" validate={required}>
-                  {({ input, meta }) => (
-                    <div>
-                      <ControlLabel>Country</ControlLabel>
-                      <InputGroup style={styles}>
-                        <InputGroup.Addon>
-                          <Icon icon="map-marker" />
-                        </InputGroup.Addon>
-                        <Input {...input} type="text" placeholder="Country" />
-                      </InputGroup>
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
+                <ControlLabel>ID</ControlLabel>
+                <InputElement type="text" icon="edit" name="id" />
+                <ControlLabel>Name</ControlLabel>
+                <InputElement type="text" icon="slack" name="name" />
+                <ControlLabel>Company</ControlLabel>
+                <InputElement type="text" icon="home" name="manufacturer" />
+                <ControlLabel>Price</ControlLabel>
+                <InputElement type="number" icon="usd" name="price" />
+                <ControlLabel>Country</ControlLabel>
+                <InputElement type="text" icon="map-marker" name="country" />
                 <div className="buttons">
                   <Button type="submit" disabled={submitting}>
-                    Submit
+                    {disableInputID ? "Update" : "Add"}
                   </Button>
                   <Button
                     type="button"
